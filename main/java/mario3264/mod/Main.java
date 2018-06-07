3f507321ec50e22583c3184d705380445593f461
Mario3264 <windowsmario64@gmail.com>
2018-06-07 14:43:09 -0700

    Create Main.java

---
+++ main/java/mario3264/mod/Main.java
@@ -0,0 +1,28 @@
+package mario3264.mod;
+
+import mario3264.mod.proxy.CommonProxy;
+import mario3264.mod.util.Reference;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class Main 
+{
+	@Instance
+	public static Main instance;
+	
+	@SidedProxy(clientSide = Reference.CLIENT, serverSide = Reference.COMMON)
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public static void preInit(FMLPreInitializationEvent event) {}
+	@EventHandler
+	public static void init(FMLInitializationEvent event) {}
+	@EventHandler
+	public static void postInit(FMLPostInitializationEvent event) {}
+}

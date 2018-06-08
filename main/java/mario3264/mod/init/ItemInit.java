70038f300d81bdd7cf5e9bf80683454dbb79515c
Mario3264 <windowsmario64@gmail.com>
2018-06-07 18:22:28 -0700

    46 more elements + other curative items

--- main/java/mario3264/mod/init/ItemInit.java
+++ main/java/mario3264/mod/init/ItemInit.java
@@ -13,7 +13,11 @@ public class ItemInit
 {
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
+
+	public static final Item ANTIDOTE = new ItemCurativePotion("antidote", 19);
 	public static final Item BLEACH = new ItemUnstackableBase("bleach");
+	public static final Item ELIXIR = new ItemCurativePotion("elixir", 18);
+	public static final Item EYE_DROPS = new ItemCurativePotion("eye_drops", 15);
 	public static final Item TONIC = new ItemCurativePotion("tonic", 9);
 }
 

1e9b8f10c7a00c49afcc8219a65d1ae15361cef6
Mario3264 <windowsmario64@gmail.com>
2019-01-21 00:28:50 -0800

    Correct version for in-game mod info.

--- main/java/mario3264/mod/util/Reference.java
+++ main/java/mario3264/mod/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MODID = "woc";
 	public static final String NAME = "World Of Chemistry";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "0.5.0";
 	public static final String CLIENT = "mario3264.mod.proxy.ClientProxy";
 	public static final String COMMON = "mario3264.mod.proxy.CommonProxy";
 }
